da056b9e777543bf0cc0b47f2cc81adade992396
avprince <shimmyaries@ALAN-TRACYs-MacBook-Air.local>
2020-12-07 15:44:56 -0600

    added JS

--- assets/main.js
+++ assets/main.js
@@ -1,38 +1,57 @@
 class Traveler {
-    constructor(name, food){
+    constructor(name){
         this.name = name;
-        this.food = food;
-        this.isHealthy = true;
+        this.food = 1; 
+        this.isHealthy = true; 
     }
     hunt(){
         this.food+=2;
     }
-    eat(meal){
-        if (this.food >=meal){
-        this.food-=meal;
-        } if (this.food===0){
-        this.isHealthy = false
+    eat(){
+        this.food -= 1;
+        if(this.food === 0) {
+            this.isHealthy = false;
+        } else {
+            this.food -= 1;
         }
     }
 }
 
+// let t = new Traveler("Name");
+// console.log(t.food);
+// t.eat();
+// console.log(t.food);
+
 class Wagon {
-    constructor(capacity, passengers){
+    constructor(capacity){
         this.capacity = capacity;
-        this.passengers = passengers;
+        this.passengers = [];
     }
-    getAvailableSeatCount(seating){
-        this.capacity -= this.passengers;
-        return seating
+    getAvailableSeatCount(){
+        return this.capacity -= this.passengers.length;
     }   
-    join(){
-        
+    join(traveler){
+        if(this.passengers.length < this.capacity){
+            this.passengers.push(traveler);
+        } 
+        // If the wagon is already at maximum capacity, don't add them.
     }
     shouldQuarantine(){
-
+        for (let i = 0; i < this.passengers.length; i++){
+            if (this.passengers[i].isHealthy === false) {
+             return true
+            }
+        }
+        return false
+        //  if there is at least one unhealthy person in the wagon. Return false if not.
     }
     totalFood(){
-
+        let total = 0;
+        for (let i = 0; i < this.passengers.length; i++) {
+            total += this.passengers[i].food;
+        } 
+        return total
+        // Returns the total amount of food among all passengers in the wagon.
     }
 }
 
